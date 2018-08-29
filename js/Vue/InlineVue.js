2b2c6beb76b4256b99f3af8932de8f58565a473a
cbowen <cbowen@cse.msu.edu>
2018-08-29 16:02:58 -0400

    New InlineVue objects. Removed some from _unsorted.scss

---
+++ js/Vue/InlineVue.js
@@ -0,0 +1,53 @@
+/**
+ * Basic Vue-based site inline content starter
+ *
+ * How to use:
+ * @code
+ * site.ready(() => {
+ *    InlineVue.create('div.cl-reviews', ReviewsVue);
+ * });
+ * @endcode
+ *
+ * @constructor
+ */
+export const InlineVue = function() {
+}
+
+/**
+ * Create an inline VUE component, replacing the provided
+ * selector. The selector is assumed to contain JSON data that is
+ * then provided to the component in the json property.
+ *
+ * @param sel Selector for a div to replace with the view.
+ * @param component Component to display (Vue)
+ */
+InlineVue.create = function(sel, component) {
+    const element = document.querySelector(sel);
+    if(element === null) {
+        return;
+    }
+
+    let template = `<div>
+<page-vue :json="json"></page-vue>
+</div>`;
+
+    const json = JSON.parse(element.textContent);
+
+    const store = Site.store;
+
+    const components = {
+            'page-vue': component
+        };
+
+    new Site.Vue({
+        el: element,
+        store,
+        data: {
+            json: json
+        },
+        template: template,
+        components: components,
+        methods: {
+        }
+    })
+}
