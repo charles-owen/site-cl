26e487a36e4797b9c3f8d65a17f32398cca7ecc5
Charles B. Owen <cbowen@msu.edu>
2022-01-07 08:58:17 -0500

    Hasher updated

--- src/Site/Util/Hasher.php
+++ src/Site/Util/Hasher.php
@@ -7,19 +7,28 @@ namespace CL\Site\Util;
 
 class Hasher {
 
-    const m = 2147483647;
-    const p = 127;
+    private $m = 2147483647;
+    private $p = 127;
 
-    public static function hash($str) {
+    private $and = 0xffffff;
+    private $p1 = 93719;
+    private $a = 21;
+    private $b = 37;
+
+    public function hash($str) {
         $h = 0;
         $pProduct = 1;
 
         for($i=0; $i<strlen($str); $i++) {
             $c = ord($str[$i]);
-            $h = ($h + $c * $pProduct) % Hasher::m;
-            $pProduct = ($pProduct * Hasher::p) % Hasher::m;
+            $h = ($h + $c * $pProduct) % $this->m;
+            $pProduct = ($pProduct * $this->p) % $this->m;
         }
 
         return $h;
+    }
+
+    public function hash2($y) {
+        return (($y & $this->and) * $this->a + $this->b) % $this->p1;
     }
 }
--- tests/phpunit/HasherTest.php
+++ tests/phpunit/HasherTest.php
@@ -6,9 +6,13 @@ use CL\Site\Util\Hasher;
 
 class HasherTest extends \PHPUnit\Framework\TestCase {
     public function test1() {
-        $this->assertEquals(597624669, Hasher::hash("student-1.0.3"));
-        $this->assertEquals(1452291392, Hasher::hash("student-1.0.1"));
-        $this->assertEquals(674232713, Hasher::hash("cbowen-1.0.3"));
+        $hasher = new Hasher();
+        $this->assertEquals(597624669, $hasher->hash("student-1.0.3"));
+        $this->assertEquals(1452291392, $hasher->hash("student-1.0.1"));
+        $this->assertEquals(674232713, $hasher->hash("cbowen-1.0.3"));
 
+        $this->assertEquals(47394, $hasher->hash2(674232713));
+        $this->assertEquals(30461, $hasher->hash2(597624669));
+        $this->assertEquals(64050, $hasher->hash2(1452291392));
     }
 }
